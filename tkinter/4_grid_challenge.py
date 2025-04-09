adf9063a0abc6318d22676cfa1b3a4e4210f1948
Savvsavv <demesasavannah@gmail.com>
2025-04-09 21:32:30 +0800

    loh

---
+++ tkinter/4_grid_challenge.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+from tkinter import *
+
+window = tk.Tk()
+window.geometry("300x300")
+window.title("Grid Method Tutorial")
+
+l1= tk.Label(window, text = "Height")
+l2= tk.Label(window, text = "Widht")
+
+#grid method to arrange labels in respective
+#rows & columns as specified
+l1.grid(row=0, column=0, sticky=W,pady=2)
+l2.grid(row=1, column=0, sticky=W,pady=2)
+
+#entry widgets ,used to take entry from user
+e1= Entry(window)
+e2= Entry(window)
+
+#this will arrange entry widgets
+e1.grid(row=0, column=1, pady=2)
+e2.grid(row=1, column=1, pady=2)
+
+#checkbutton widget
+c1= Checkbutton(window,text ="Preserve")
+c1.grid(row=2, column=0, sticky=W, columnspan=2)
+
+#adding image (remember image should be PNG and not JPG)
+img= tk.PhotoImage(file =r"C:\Users\savannah\Desktop\smth\tkinter")
+img1= img.subsample(2,2)
+
+#setting image with the help of label
+Label(window, image= img1).grid(row = 0, column=2, columnspan= 2, rowspan = 2, padx= 5, pady=5)
+
+#button widget
+b1= tk.Button(window,text ='Zoom IN')
+b2= tk.Button(window,text ='Zoom OUT')
+
+#arranging button widgets
+b1.grid(row=2, column=2, sticky=E)
+b2.grid(row=2, column=3, sticky=E)
+
+window.mainloop()
