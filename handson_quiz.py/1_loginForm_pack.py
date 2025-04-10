e8fcdc7d69adf79a17f7954f446a7f200975e8b0
Savvsavv <demesasavannah@gmail.com>
2025-04-10 22:04:53 +0800

    commit

---
+++ handson_quiz.py/1_loginForm_pack.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+from tkinter import *
+
+
+window = tk.Tk()
+window.geometry("300x300")
+window.title("LOG IN ")
+
+label = tk.Label(window, text="LOG IN")
+label.pack(pady=10)
+
+entry= tk.Entry()
+entry.pack()
+entry2= tk.Entry()
+entry2.pack(pady=10)
+
+b1=tk.Button(window, text="Log In", bg="blue",fg="white", width=16)
+b1.pack()
+
+cb1=tk.Checkbutton(window, text="Remember Me?")
+cb1.pack()
+window.mainloop()
