91815ba9c4e5a67207a7f20802addae9f2894f96
Savvsavv <demesasavannah@gmail.com>
2025-04-11 22:26:51 +0800

    rr

--- handson_quiz.py/1_loginForm_pack.py
+++ handson_quiz.py/1_loginForm_pack.py
@@ -6,26 +6,42 @@ window = tk.Tk()
 window.geometry("300x300")
 window.title("LOG IN ")
 
-label = tk.Label(window, text="LOG IN")
+#frame
+frame=tk.Frame()
+s_frame=tk.Frame()
+
+#log in
+label = tk.Label(frame, text="LOG IN", font=("Arial",10))
 label.pack(pady=10)
 
-entry= tk.Entry()
+#entry
+entry= tk.Entry(frame)
 entry.insert(0,"Username")
 entry.pack()
-entry2= tk.Entry()
-entry2.insert(0,"Password")
+entry2= tk.Entry(frame)
+entry2.insert( 0,"Password")
 entry2.pack(pady=10)
 
-b1=tk.Button(window, text="Log In", bg="blue",fg="white", width=16)
+b1=tk.Button(frame, text="Log In", bg="blue",fg="white", width=16, font=("Arial",10))
 b1.pack()
 
-cb1=tk.Checkbutton(window, text="Remember Me?")
-cb1.pack(side=LEFT)
+cb=tk.IntVar()
+cb=tk.Checkbutton(frame, text="Remember Me?", font=("Arial",7), variable=cb)
+cb.pack(side=LEFT,pady=2)
 
-label=tk.Label(window, text="Forgot Password?", fg="blue")
-label.pack(side=RIGHT)
+label=tk.Label(frame, text="Forgot Password?", fg="blue", font=("Arial",7))
+label.pack(side=RIGHT, pady=2)
 
-lb2=tk.Label(window, text= "Create an Account", fg="blue")
-lb2.pack()
+canvas = tk.Canvas(s_frame, width=40, height=10)
+canvas.pack()
+canvas_height = 20
+canvas_width = 200
+y = int(canvas_height / 2)
+canvas.create_line(0, y, canvas_width, y)
 
+lb2=tk.Label(s_frame, text= "Create an Account", fg="blue", font=("Arial",7))
+lb2.pack(pady=2)
+
+frame.pack()
+s_frame.pack()
 window.mainloop()
